4918d0bbf6e62e6d0e652c2d1c53b169fdc06a43
FiXsyy <f0n1x3r@gmail.com>
2025-04-15 11:20:35 +0200

    Added a more info button to /coinflip
    Added a more info button, that shows a detailed list of flips on press
    
    Added comments for better clarity

--- commands/chance/coinflip.js
+++ commands/chance/coinflip.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { coinImg } = require('../../config.json')
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { coinImg } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,15 +12,15 @@ module.exports = {
     async execute(interaction){
         const flip_number = interaction.options.getInteger('flip_number') ?? 1;
         
+        //  Creates a new embed
         const embed = new EmbedBuilder()
-                    .setColor(0xffcc00)
-                    .setTitle('Title')
-                    .setDescription('Description')
-                    .setThumbnail(coinImg);
+            .setColor(0xffcc00)
+            .setTitle('Title')
+            .setDescription('Description')
+            .setThumbnail(coinImg);
 
-        var total_heads = 0;
-        var rolls = [];
-
+        //  If flip_number == 1
+        //  Sends a siple embed
         if(flip_number == 1){
             let newEmbed = EmbedBuilder.from(embed)
                 .setTitle('Results for 1 Flip')
@@ -29,15 +29,45 @@ module.exports = {
             return;
         }
 
+        //  If flip_number > 1
+        //  Creates an info button to attach to the embed
+        const info = new ButtonBuilder()
+            .setCustomId('info')
+            .setLabel('More Info')
+            .setStyle(ButtonStyle.Primary);
+
+        const row = new ActionRowBuilder()
+            .addComponents(info);
+
+        //  Randomises flips, adds them to rolls and calculates total_heads
+        var total_heads = 0;
+        var rolls = [];
+
         for(let i = 0; i < flip_number; ++i){
             let roll = Math.floor(Math.random() * 2);
             total_heads += roll;
             rolls.push(roll ? 'Heads' : 'Tails');
         }
 
+        //  Sends an embed with an attached info button
         let newEmbed = EmbedBuilder.from(embed)
             .setTitle(`Results for ${flip_number} Flips`)
-            .setDescription(`**You flipped:**\n${rolls.map(value => value).join(', ')}\n\n**Total:** ${total_heads} Heads, ${flip_number - total_heads} Tails`); 
-        await interaction.reply({embeds: [newEmbed]});
+            .setDescription(`**Total:** ${total_heads} Heads, ${flip_number - total_heads} Tails`); 
+        const response = await interaction.reply({embeds: [newEmbed], components: [row], withResponse: true});
+
+        //  Awaits button press
+        const collectionFilter = i => i.user.id === interaction.user.id;
+        try{
+            const confirmation = await response.resource.message.awaitMessageComponent({filter: collectionFilter, time: 60_000});
+
+            if(confirmation.customId === 'info'){
+                let descriptiveEmbed = EmbedBuilder.from(newEmbed)
+                    .setDescription(`**You flipped:**\n${rolls.map(value => value).join(', ')}\n\n**Total:** ${total_heads} Heads, ${flip_number - total_heads} Tails`); 
+                await confirmation.update({embeds: [descriptiveEmbed], components: []});
+            }
+        }
+        catch{
+            await interaction.editReply({components: []});
+        }
     },
 };
